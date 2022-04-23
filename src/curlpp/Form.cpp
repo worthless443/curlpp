eff2f133e2760323e8723acc2bf660799bffb414
worthless443 <aissypewdiepie@gmail.comn>
2022-04-24 03:16:54 +0530

    playing around

--- src/curlpp/Form.cpp
+++ src/curlpp/Form.cpp
@@ -22,8 +22,12 @@
  */
 
 #include "curlpp/Form.hpp"
-
+#include <boost/algorithm/string/join.hpp>
 #include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <stdlib.h>
+#include <vector>
 
 curlpp::HttpPost::HttpPost(const Forms & posts)
   : mFirst(NULL)
@@ -53,7 +57,7 @@ curlpp::HttpPost::operator=(const Forms & posts)
 {
   clear();
 
-  Forms::const_iterator pos;
+  curlpp::Forms::const_iterator pos;
   for(pos = posts.begin(); pos != posts.end(); pos++) 
   {
     mForms.push_back(*pos);
@@ -84,7 +88,7 @@ curlpp::HttpPost::clear()
 curlpp::Forms curlpp::HttpPost::getList()
 {
   //I'm not sure cloning is absolutely necessary.
-  Forms newForm;
+  curlpp::Forms newForm;
 
   Forms::const_iterator pos;
   for(pos = mForms.begin(); pos != mForms.end(); pos++) 
@@ -94,7 +98,7 @@ curlpp::Forms curlpp::HttpPost::getList()
 }
 
 curlpp::FormPart::FormPart(const char * name)
-  : mName(name)
+  : mName(name) 
 {}
 
 curlpp::FormPart::~FormPart()
@@ -107,6 +111,8 @@ curlpp::FormPart::FormPart(const std::string & name)
 curlpp::FormParts::File::File(const char * name, const char * filename)
   : FormPart(name)
   , mFilename(filename)
+  ,mContentType(mFilename.c_str())
+
 {}
 
 curlpp::FormParts::File::~File()
@@ -129,15 +135,16 @@ curlpp::FormParts::File::File(const std::string & name,
 			      const std::string & contentType)
   : FormPart(name)
   , mFilename(filename)
-  , mContentType(contentType)
+  , mContentType(contentType) 
 {}
 
 curlpp::FormParts::File *
 curlpp::FormParts::File::clone() const
 {
+   mFilename;	
    return new curlpp::FormParts::File(* this);
 }
-
+  
 void
 curlpp::FormParts::File::add(::curl_httppost ** first, 
 			     ::curl_httppost ** last)
@@ -166,12 +173,38 @@ curlpp::FormParts::File::add(::curl_httppost ** first,
   }
 }
 
+curlpp::test::test(const char* name, const char *fname) : f(name, fname), File(name, fname){}
+std::vector<std::string> curlpp::test::add(const std::string s) {
+	char *chr = new char;
+	char *chr_main = new char;
+	
+	std::vector<std::string> v;
+	std::vector<std::string> v_main;
+
+	std::string *str_main = (std::string*)malloc(1000);//new std::string;//= new std::string{""};//= new std::back_insert_iterator<std::string>(Str);
+	int ii=0, kk=0;
+	for(auto i=mContentType.begin();i<mContentType.end();i++) {
+		__gnu_cxx::__normal_iterator<const char*, std::string> biter;//j:w
+		__gnu_cxx::__normal_iterator<const char*, std::string> eiter = mFilename.end();//j:w
+		for(auto jb = mFilename.begin();jb<mFilename.end();jb++) {
+			__gnu_cxx::__normal_iterator iter = s.begin() + kk;
+			v.push_back(std::string{*i} + *jb + *iter);
+		}
+		
+		ii=0;
+		v_main.push_back(std::string{chr[kk]} + std::string{chr[kk]});
+		kk+=1;
+	}
+	return v_main;
+}
 
 curlpp::FormParts::Content::Content(const char * name, 
 				    const char * content)
   : FormPart(name)
   , mContent(content)
 {}
+
+
 
 curlpp::FormParts::Content::~Content()
 {}
@@ -203,8 +236,7 @@ curlpp::FormParts::Content::clone() const
    return new curlpp::FormParts::Content(*this);
 }
 
-void
-curlpp::FormParts::Content::add(::curl_httppost ** first, 
+void curlpp::FormParts::Content::add(::curl_httppost ** first, 
 				::curl_httppost ** last)
 {
   // We uses options that will copy internally the string (c_str),
@@ -213,7 +245,7 @@ curlpp::FormParts::Content::add(::curl_httppost ** first,
     curl_formadd(first, 
 		 last, 
 		 CURLFORM_PTRNAME, 
-		 mName.c_str(), 
+		 mName.c_str(),
 		 CURLFORM_PTRCONTENTS,
 		 mContent.c_str(),
 		 CURLFORM_END);
